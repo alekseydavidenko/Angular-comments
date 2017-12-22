39930fc5984da18ed8d6fdb410864271b68d571d
Aleksey Davidenko <aleksey_davidenko@ukr.net>
2017-12-22 11:59:45 +0200

    v.0.0.2

---
+++ app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+
+import { AppComponent } from "./app.component";
+import { routes } from "./app.routes";
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(routes),
+    ],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
